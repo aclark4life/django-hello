303b33421315d5a0e2a831963075f097a33296d3
Alex Clark <aclark@aclark.net>
2012-10-23 09:52:16 -0400

    Docstrings

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 import datetime
 
 
+# Based on https://docs.djangoproject.com/en/1.4/topics/http/views/
 def index(request):
+    """
+    This function provides a 'Hello World' response by wrapping 
+    some HTML in a Django response
+    """
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
